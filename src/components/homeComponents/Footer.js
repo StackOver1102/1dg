5fe394a63f750b51b1ad75c1b915888da21edcc0
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2024-06-30 20:34:37 +0700

    update

--- src/components/homeComponents/Footer.js
+++ src/components/homeComponents/Footer.js
@@ -2,89 +2,60 @@ import React from 'react'
 import logo from "../../assets/images/logo.png"
 function Footer() {
     return (
-        <div className='mt-0'>
-            <div className='relative landing-dark-color '>
-                <svg viewBox="15 -1 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 48C4.93573 47.6644 8.85984 47.3311 12.7725 47H1489.16C1493.1 47.3311 1497.04 47.6644 1501 48V47H1489.16C914.668 -1.34764 587.282 -1.61174 12.7725 47H1V48Z" fill="currentColor"></path>
-                </svg>
-            </div>
-            <div className='bg-[#13263c] pt-20'>
-                <div className='py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12'>
-                    <div className="mt-12 grid grid-auto-fit-lg gap-8 ">
-                        <div className='flex flex-col'>
-                            <div className="rounded-lg landing-dark-border p-9 mb-10 ">
-                                <h2 className="text-white text-2xl">Would you need more infomation?</h2>
-                                <span className="font-normal text-xl  text-gray-700">Email us to
-                                    <a href="#" className="text-white opacity-50 text-hover-primary"> admin@1dg.me</a></span>
-                            </div>
-                            <div className="rounded-lg landing-dark-border p-9 mb-10 ">
-                                <h2 className="text-white text-2xl">Do you need reseller price?</h2>
-                                <span className="font-normal text-xl  text-gray-700">Email us to
-                                    <a href="#" className="text-white opacity-50 text-hover-primary"> admin@1dg.me</a></span>
-                            </div>
+        <div className="mt-0">
+        <div className="relative bg-[#13263c] landing-dark-color">
+            <svg viewBox="15 -1 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 48C4.93573 47.6644 8.85984 47.3311 12.7725 47H1489.16C1493.1 47.3311 1497.04 47.6644 1501 48V47H1489.16C914.668 -1.34764 587.282 -1.61174 12.7725 47H1V48Z" fill="currentColor"></path>
+            </svg>
+        </div>
+        <div className="bg-[#13263c] pt-20">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-12">
+                <div className="mt-12 grid lg:grid-cols-2 gap-8">
+                    <div className="flex flex-col">
+                        <div className="rounded-lg border border-[#2e4b66] p-9 mb-10 bg-[#1a3a5c]">
+                            <h2 className="text-white text-2xl mb-4">Would you need more information?</h2>
+                            <span className="font-normal text-xl text-gray-300">Email us at
+                                <a href="mailto:tuantrangptkm@gmail.com" className="text-white opacity-75 hover:text-[#009ef7]"> tuantrangptkm@gmail.com</a>
+                            </span>
                         </div>
-                        <div className='ml-48 lg:ml-72'>
-                            <div className='flex justify-center'>
-                                <div className="flex font-medium flex-col mr-20">
-                                    <h4 className="font-semibold text-gray-400 mb-6">More</h4>
-
-                                    <a href="/faqs" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">FAQ</a>
-
-                                    <a href="/terms" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">Terms</a>
-
-                                    <a href="/services" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">Services</a>
-
-                                    <a href="/api" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">API</a>
-
-                                </div>
-                                {/* <div className="flex font-medium flex-col lg:ml-20">
-                                    <h4 className="font-semibold text-gray-400 mb-6">Contact Us</h4>
-                                
-                                    <a href="https://www.facebook.com/ntu11022002/" className="mb-6">
-                                        <img src="https://1dg.me/assets/client//media/svg/brand-logos/facebook-4.svg" className="h-[20px] mr-2" alt=""/>
-                                            <span className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">https://www.facebook.com/ntu11022002/</span>
-                                    </a>
-                                 
-                                    <a href="#" className="mb-6">
-                                        <img src="/assets/client//media/svg/brand-logos/telegram.svg" className="h-20px me-2" alt=""/>
-                                            <span className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">@lethang1dg</span>
-                                    </a>
-                                    
-                                    <a href="#" className="mb-6">
-                                        <img src="/assets/client//media/svg/brand-logos/phone.svg" className="h-20px me-2" alt=""/>
-                                            <span className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">099.73.66666</span>
-                                    </a>
-                                </div> */}
-                                <div className="flex font-medium flex-col mr-20">
-                                    <h4 className="font-semibold text-gray-400 mb-6">Contact Us</h4>
-
-                                    <a href="/faqs" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">
-                                        https://www.facebook.com/ntu11022002/
-
-                                    </a>
-
-                                    <a href="/terms" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">@lethang1dg</a>
-
-                                    <a href="/services" className="text-white opacity-50 hover:text-[#009ef7] text-[1.15rem] mb-6">099.73.66666</a>
-
-
-                                </div>
+                        <div className="rounded-lg border border-[#2e4b66] p-9 mb-10 bg-[#1a3a5c]">
+                            <h2 className="text-white text-2xl mb-4">Do you need reseller price?</h2>
+                            <span className="font-normal text-xl text-gray-300">Email us at
+                                <a href="mailto:tuantrangptkm@gmail.com" className="text-white opacity-75 hover:text-[#009ef7]"> tuantrangptkm@gmail.com</a>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="lg:ml-24">
+                        <div className="flex justify-center">
+                            <div className="flex flex-col mr-20">
+                                <h4 className="font-semibold text-gray-400 mb-6">More</h4>
+                                <a href="/faqs" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">FAQ</a>
+                                <a href="/terms" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">Terms</a>
+                                <a href="/services" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">Services</a>
+                                <a href="/api" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">API</a>
+                            </div>
+                            <div className="flex flex-col">
+                                <h4 className="font-semibold text-gray-400 mb-6">Contact Us</h4>
+                                <a href="/faqs" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">Phan Thanh Thao</a>
+                                <a href="/terms" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">tuantrangptkm@gmail.com</a>
+                                <a href="/services" className="text-white opacity-75 hover:text-[#009ef7] text-lg mb-4">+1 980286796</a>
                             </div>
                         </div>
                     </div>
-
                 </div>
-                <div className='landing-dark-separator'></div>
-                <div className='py-8 px-4 mx-auto max-w-screen-xl  lg:py-10 lg:px-12'>
-                    <div className='flex items-center order-2 nd:order-1'>
-                        <a href="/">
-                            <img alt="Logo" src="https://1dg.me/assets/media/favicon.ico" className="h-[15px] md:h-[20px]" />
-                        </a>
-                        <span className="mx-5 text-[1.075rem] font-medium text-gray-600 pt-1" href="https://1dg.me">© 2022 1DG SMM Panel - Social Services</span>
-                    </div>
+            </div>
+            <div className="border-t border-[#2e4b66] mt-10"></div>
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-10 lg:px-12">
+                <div className="flex items-center justify-between">
+                    <a href="/">
+                        <img alt="Logo" src="https://1dg.me/assets/media/favicon.ico" className="h-5 md:h-6" />
+                    </a>
+                    <span className="text-gray-600 text-base md:text-lg">© 2022 1DG SMM Panel - Social Services</span>
                 </div>
             </div>
         </div>
+    </div>
+    
     )
 }
 
